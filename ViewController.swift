ef0fb4d714b9a15a65557ec1c20102c2a61ade5d
LemonaInc <jaxonstevens@lemonainc.com>
2017-09-08 16:40:18 -0700

    Network Connection Error Popup Added

--- ViewController.swift
+++ ViewController.swift
@@ -14,10 +14,19 @@ class ViewController: UIViewController, UIWebViewDelegate   {
     // Student Web view for reports
     @IBOutlet weak var studentWebView: UIWebView!
     
+   var networkint = 1
+
+    
+    // VIEW DID LOAD METHOD
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
+        
+        let timer = Timer.scheduledTimer(
+            timeInterval: 2.0, target: self, selector: #selector(ViewController.viewDidAppear(_:)),
+            userInfo: nil, repeats: true)
+        
         // Set the webview delgate to self
         studentWebView.delegate = self
         
@@ -35,13 +44,13 @@ class ViewController: UIViewController, UIWebViewDelegate   {
     // VIEW DID APPEAR METHOD -- USE THIS FOR SHOWING ALERT VIEWS
     override func viewDidAppear(_ animated: Bool) {
         
-        
+    
         // If the network connection is true the do
         if NetworkReachability.isConnectedToNetwork() == true
         {
-            
+            networkint = 1
+
             print("Network Reached")
-            
             
             let alert = UIAlertController(title: "You are connected to a network", message:"Thanks", preferredStyle: UIAlertControllerStyle.alert); alert.addAction(UIAlertAction(title:"Okay",style:UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
@@ -52,13 +61,17 @@ class ViewController: UIViewController, UIWebViewDelegate   {
             // If the network conenction cannot be reached or fails then do
         else
         {
+           // networkint = 0
+
+          //  while networkint == 0 {
             
-            print("Network Error")
+           // print("Network Error")
             
             let alert = UIAlertController(title: "Network Error", message:"Unfortunatly you dont have a connection at this time", preferredStyle: UIAlertControllerStyle.alert); alert.addAction(UIAlertAction(title:"Okay I Understand",style:UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
             
             
+         //   }
         }
 
     }
